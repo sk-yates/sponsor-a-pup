d583f4a2e57598b0f3ceeb6d8f8eda6b20fe872a
sk-yates <syates.edu@gmail.com>
2025-02-27 21:52:03 +0000

    Added: Puppies Model & Base-level navigation

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Puppies(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    age = models.IntegerField()
+    description = models.TextField(max_length=500)
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,15 @@ from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
     # Routes will be added here
-    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
+    
     path('my-details/', views.user_details, name='my-details'),
+    
     path('pupdates/', views.pupdates, name='pupdates'),
+    path('pupdates-details/', views.pupdates_details, name='pupdates-details'),
+
+    path('my-sponsorship/', views.my_sponsorship, name='my-sponsorship'),
+    path('my-subscription/', views.my_subscription, name='my-subscription'),
+
+
 ]
