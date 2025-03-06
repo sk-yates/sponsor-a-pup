558f9d8260509576f436933aaa157752039e7093
sk-yates <syates.edu@gmail.com>
2025-03-06 19:00:36 +0000

    Alex test

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Puppy
+from .models import Puppy, SponsorUser
 
 # Register your models here.
 
 admin.site.register(Puppy)
+
+admin.site.register(SponsorUser)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.urls import reverse
 from django.contrib.auth.hashers import make_password
 
@@ -17,9 +17,11 @@ class Puppy(models.Model):
     def get_absolute_url(self):
         return reverse('pup-profile', kwargs={'pup_id': self.id})
     
-class User(models.Model):
-    user_email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=100, validators=[MinLengthValidator(8)])
+
+class SponsorUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    groups = models.ManyToManyField(Group, related_name='customuser_set', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='customuser_set', blank=True)
     pronouns = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -28,8 +30,26 @@ class User(models.Model):
     spon_pups = models.ManyToManyField('Puppy')
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def save(self, *args, **kwargs):
-        # Automatically hash the password on creation if it's not already hashed
-        if not self.pk and not self.password.startswith('pbkdf2_'):
-            self.password = make_password(self.password)
-        super().save(*args, **kwargs)
+    def __str__(self):
+        return self.username
+
+
+# # class User(models.Model):
+#     user_email = models.EmailField(max_length=100)
+#     password = models.CharField(max_length=100, validators=[MinLengthValidator(8)])
+#     pronouns = models.CharField(max_length=100)
+#     first_name = models.CharField(max_length=100)
+#     last_name = models.CharField(max_length=100)
+#     user_tel = models.CharField(max_length=20)
+#     contact_pref = models.JSONField(default=list)
+#     spon_pups = models.ManyToManyField('Puppy')
+#     created_at = models.DateTimeField(auto_now_add=True)
+    # is_seller = models.BooleanField(default=False)
+    # profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
+
+
+#     def save(self, *args, **kwargs):
+#         # Automatically hash the password on creation if it's not already hashed
+#         if not self.pk and not self.password.startswith('pbkdf2_'):
+#             self.password = make_password(self.password)
+#         super().save(*args, **kwargs)
