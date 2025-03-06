f5bacebf63a36e7dd9f20094facf4dceaf8d2a90
sk-yates <syates.edu@gmail.com>
2025-03-06 19:27:43 +0000

    user model updated / user field added to puppy model / pupdate model  added

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Puppy, SponsorUser
+from .models import SponsorUser, Puppy, Pupdate
 
 # Register your models here.
 
 admin.site.register(Puppy)
-
+admin.site.register(Pupdate)
 admin.site.register(SponsorUser)
--- main_app/models.py
+++ main_app/models.py
@@ -4,18 +4,7 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.urls import reverse
 from django.contrib.auth.hashers import make_password
 
-class Puppy(models.Model):
-    name = models.CharField(max_length=100)
-    breed = models.CharField(max_length=100)
-    age = models.IntegerField()
-    description = models.TextField(max_length=500)
-    location = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
-    
-    def get_absolute_url(self):
-        return reverse('pup-profile', kwargs={'pup_id': self.id})
     
 
 class SponsorUser(AbstractUser):
@@ -33,23 +22,42 @@ class SponsorUser(AbstractUser):
     def __str__(self):
         return self.username
 
-
-# # class User(models.Model):
-#     user_email = models.EmailField(max_length=100)
-#     password = models.CharField(max_length=100, validators=[MinLengthValidator(8)])
-#     pronouns = models.CharField(max_length=100)
-#     first_name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-#     user_tel = models.CharField(max_length=20)
-#     contact_pref = models.JSONField(default=list)
-#     spon_pups = models.ManyToManyField('Puppy')
-#     created_at = models.DateTimeField(auto_now_add=True)
-    # is_seller = models.BooleanField(default=False)
-    # profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
-
-
 #     def save(self, *args, **kwargs):
 #         # Automatically hash the password on creation if it's not already hashed
 #         if not self.pk and not self.password.startswith('pbkdf2_'):
 #             self.password = make_password(self.password)
 #         super().save(*args, **kwargs)
+# 
+# 
+# 
+
+
+class Puppy(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    age = models.IntegerField()
+    description = models.TextField(max_length=500)
+    location = models.CharField(max_length=100)
+
+    user = models.ManyToManyField(SponsorUser, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('pup-profile', kwargs={'pup_id': self.id})
+
+
+#   # profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
+
+class Pupdate(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField(max_length=300)
+    picture_url = models.ImageField(max_length=400)
+    media_url = models.CharField(max_length=400)
+    date = models.DateField('Pupdate date')
+
+    pup = models.ForeignKey(Puppy, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Pupdate {self.title} on {self.date}"
